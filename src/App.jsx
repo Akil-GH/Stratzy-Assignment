a32fbfef3b2442321b780a11d1565625ddf77440
Akil-GH <akildesai7@gmail.com>
2024-03-30 16:33:31 +0530

    first commit
    Description

---
+++ src/App.jsx
@@ -0,0 +1,260 @@
+
+import React from 'react';
+import './App.css';
+import twit from './images/arktwit.jpg'
+import inst from './images/images.png'
+import what from './images/wp.jpg'
+import linked from './images/download.png'
+import loss from  './images/loss.png'
+
+function App() {
+  return (
+        <form className="sidebar-cards-trade-added">
+          <div className="sidebar-cards-trade-added-inner">
+            <header className="frame-parent">
+              <div className="invest-wrapper">
+                <div className="invest">Invest</div>
+              </div>
+              <button className="document-prep">
+                <b className="trade">Trade</b>
+              </button>
+            </header>
+          </div>
+          <div className="order-type">
+            <div className="products">
+              <div className="limit-sell">
+                <div className="pngwing-1-wrapper">
+                  <img className="pngwing-1-icon" alt="" src={loss} />
+                </div>
+                <div className="sequences">
+                  <b className="tata-steel">TATA STEEL</b>
+                  <div className="tickername">{`{TickerName}`}</div>
+                </div>
+              </div>
+              <div className="parent">
+                <b className="b">₹ 6451.50</b>
+                <div className="frame-wrapper">
+                  <div className="icon-parent">
+                    <img className="icon"  src={loss} />
+                    <div className="usa-eshi">10.8 (1.65%)</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <section className="limit-sell-active">
+            <div className="order-type-parent">
+              <div className="order-type1">Order Type</div>
+              <div className="frame-group">
+                <div className="radio-buttons-dark-parent">
+                  <div className="radio-buttons-dark">
+                    <div className="container">
+                      <div className="state-layer">
+                        <img
+                          className="icon1"
+                          loading="lazy"
+                          alt=""
+                          src="/icon-1.svg"
+                        />
+                      </div>
+                    </div>
+                  </div>
+                  <div className="market">Market</div>
+                </div>
+                <div className="application-id">
+                  <div className="radio-buttons-dark1">
+                    <div className="container1">
+                      <div className="state-layer1">
+                        <img
+                          className="icon2"
+                          loading="lazy"
+                          alt=""
+                          src="/icon-2.svg"
+                        />
+                      </div>
+                    </div>
+                  </div>
+                  <div className="limit">Limit</div>
+                </div>
+              </div>
+            </div>
+            <div className="action-type-parent">
+              <div className="action-type">Action Type</div>
+              <div className="frame-container">
+                <div className="radio-buttons-dark-group">
+                  <div className="radio-buttons-dark2">
+                    <div className="container2">
+                      <div className="state-layer2">
+                        <img
+                          className="icon3"
+                          loading="lazy"
+                          alt=""
+                          src="/icon-3.svg"
+                        />
+                      </div>
+                    </div>
+                  </div>
+                  <div className="buy">Buy</div>
+                </div>
+                <div className="radio-buttons-dark-container">
+                  <div className="radio-buttons-dark3">
+                    <div className="container3">
+                      <div className="state-layer3">
+                        <img
+                          className="icon4"
+                          loading="lazy"
+                          alt=""
+                          src="/icon-4.svg"
+                        />
+                      </div>
+                    </div>
+                  </div>
+                  <div className="sell">Sell</div>
+                </div>
+              </div>
+            </div>
+          </section>
+          <section className="m-parent">
+            <div className="m" />
+            <div className="card-separator-parent">
+              <div className="card-separator">
+                <div className="share-on-label">
+                  <div className="shares">Shares</div>
+                </div>
+                <div className="rectangle-parent">
+                  <div className="frame-child" />
+                  
+                  
+                  <div className="whatsapp-icon">12</div>
+                  <div className="broker-connect-icon">
+                   
+                  </div>
+                </div>
+              </div>
+              <div className="docfileicon">
+                <div className="price-wrapper">
+                  <div className="price">Price</div>
+                </div>
+                <button className="frame-button">
+                  <div className="wrapper">
+                    <b className="b1">₹ 6451.50</b>
+                  </div>
+                  <div className="frame-item" />
+                </button>
+              </div>
+              <div className="name">
+                <div className="stop-loss-parent">
+                  <div className="stop-loss">Stop Loss</div>
+                  <div className="paul-rudd">
+                    <button className="version">
+                      <div className="div">2.0%</div>
+                    </button>
+                    <button className="life-cycle">
+                      <div className="general">1.5%</div>
+                    </button>
+                    <button className="created-on">
+                      <div className="utc">1.0%</div>
+                    </button>
+                    <div className="created-on1">
+                      <div className="utc1">0.5%</div>
+                    </div>
+                  </div>
+                </div>
+                <div className="language">
+                  <b className="b2">--</b>
+                </div>
+              </div>
+              <div className="ready-for-publishing">
+                <div className="total-trade-val">
+                  <div className="s-e-b-i-reg-no-i-n-h">
+                    <div className="target">Target</div>
+                    <button className="tags">
+                      <div className="div2">25%</div>
+                    </button>
+                  </div>
+                  <div className="bookmarks-edited-wrapper">
+                    <button className="bookmarks-edited">
+                      <div className="yes">50%</div>
+                    </button>
+                  </div>
+                  <div className="total-trade-val-inner">
+                    <div className="frame">
+                      <div className="div2">75%</div>
+                    </div>
+                  </div>
+                  <div className="data-merger">
+                    <div className="max">MAX</div>
+                  </div>
+                </div>
+                <div className="primary-buttons-wrapper">
+                  <b className="primary-buttons">--</b>
+                </div>
+              </div>
+            </div>
+          </section>
+          <div className="sidebar-cards-trade-added-child">
+            <div className="frame-inner" />
+          </div>
+          <div className="matrix-transformer-wrapper">
+            <div className="matrix-transformer">
+              <div className="share-on-label1">
+                <div className="total-trade-value">Total Trade Value</div>
+              </div>
+              <div className="group-parent">
+                <img className="group-child" alt="" src="/group-30538.svg" />
+                <div className="brokerage-fee">Brokerage Fee : $15.02</div>
+              </div>
+              <div className="div3">₹43,343</div>
+            </div>
+          </div>
+          <footer className="broker-connect-instance">
+            <button className="primary-buttons1">
+              <b className="start-investing">Create Buy Trade</b>
+            </button>
+            <div className="broker-connect-instance-inner">
+              <div className="frame-div">
+                
+                <div className="sebi">
+                  <div className="sebi-reg-no">SEBI Reg. No. INH000009180</div>
+                </div>
+              </div>
+            </div>
+          </footer>
+          
+            <div className="share-on-parent">
+              <div className="share-on">Share On</div>
+              <div className="social-media-parent">
+                <div className="social-media">
+                  <img
+                    className="instagram-color-icon-1"
+                    src={inst}
+                  />
+                </div>
+                <div className="social-media">
+                  <img
+                    className="instagram-color-icon-1"
+                    src={twit}
+                  />
+                </div>
+                <div className="social-media">
+                  <img
+                    className="instagram-color-icon-1"
+                    
+                    src={linked}
+                  />
+                </div>
+                <div className="social-media">
+                  <img
+                    className="instagram-color-icon-1"
+                    src={what}
+                  />
+                </div>
+               
+              </div>
+            </div>
+          
+        </form>
+      );
+  }
+
+export default App;
